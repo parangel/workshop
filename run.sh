226fb906da0788f864c5d191742d70b30184379e
parangel <pedroassuncaorangel@gmail.com>
2022-05-05 17:54:25 -0400

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --account=def-cdesouza
+#SBATCH --time=00:01:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=1G
+#SBATCH -o ./test.out # STDOUT
+
+module load r/4.0.0
+Rscript test.r
+
